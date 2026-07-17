c01bce455fd70d4e99b768f657c21363e2256731
samiseyam58-cell <samiseyam58@gmail.com>
2026-07-17 10:29:21 +0500

    Refactor fetch event listener in sw.js
    Updated fetch event listener syntax and added comment.

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-self.addEventListener('fetch', function(event) {
-  event.respondWith(fetch(event.request));
+self.addEventListener('fetch', (event) => {
+    // Service Worker active hona zaroori hai
 });
